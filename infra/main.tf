f16637d69f6824cbbd42c58ffa4ac49f4fb83d14
maqar424 <kolja.grosse@gmail.com>
2026-05-12 15:23:42 +0200

    DNS update

--- infra/main.tf
+++ infra/main.tf
@@ -258,3 +258,19 @@ output "cloudfront_url" {
 output "api_url" {
   value = "${aws_apigatewayv2_api.http_api.api_endpoint}/gallery"
 }
+
+resource "aws_route53_zone" "main" {
+  name = "high-definition.net"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "high-definition.net"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
